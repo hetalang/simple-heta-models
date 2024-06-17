02113ec4c15475090f9f48591290d211be828c31
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2024-06-17 10:26:14 +0300

    remove key

--- test/sbml-cases.js
+++ test/sbml-cases.js
@@ -1,25 +1,24 @@
 // project level KEYS 
 // my-service-account openai-test
-// setx OPENAI_API_KEY "sk-my-service-account-I1zZPhGR2Byyk4fYHeS8T3BlbkFJ1ccT1hCLQqLBnX1xKb51"
+// setx OPENAI_API_KEY "sk-my-service-account-"
 
 const { OpenAI } = require('openai');
 
 const openai = new OpenAI({
     organization: "org-V73x0GwuiyLqNseJ9DOyGMjy",
     project: "proj_tSNHinT8kJJl8LMqqG8X7HQp",
-    apiKey: "sk-my-service-account-I1zZPhGR2Byyk4fYHeS8T3BlbkFJ1ccT1hCLQqLBnX1xKb51"
+    apiKey: "sk-my-service-account-"
 });
 
 async function main() {
     const completion = await openai.completions.create({
-        model: 'ft:davinci-002:personal:siple-heta:9alRfxFv', // davinci-002, gpt-3.5-turbo-instruct, babbage-002, ft:davinci-002:personal:sbml-heta:9ZOUT9dV
+        // davinci-002, gpt-3.5-turbo-instruct, babbage-002 - default models
+        model: 'ft:babbage-002:personal:simple-heta:9am9znES', // ft:babbage-002:personal:simple-heta:9am9znES
         //prompt: "What is the capital of the United Kingdom?",
-        prompt: `Create Heta model with stoichiometry 1A + 2D <=> 3C + 4F. No export.`,
+        prompt: `Create complete Heta model with stoichiometry 1A + 2D <=> 3C + 4F.`,
         max_tokens: 1024,
-        temperature: 0.1,
+        temperature: 0.0,
     });
     console.log(completion.choices[0].text);
 }
 main();
-
-//console.log(openai);
