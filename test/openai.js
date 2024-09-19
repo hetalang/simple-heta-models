c27e5eff8c2225770a356ab081f384519fbb78d2
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2024-09-19 10:32:40 +0300

    fix openai test

--- test/openai.js
+++ test/openai.js
@@ -1,13 +1,13 @@
-// project level KEYS 
-// my-service-account openai-test
-// setx OPENAI_API_KEY "sk-my-service-account-"
+require('dotenv').config();
+
+const { OPENAI_ORGANIZATION, OPENAI_PROJECT, OPENAI_API_KEY} = process.env;
 
 const { OpenAI } = require('openai');
 
 const openai = new OpenAI({
-    organization: "org-V73x0GwuiyLqNseJ9DOyGMjy",
-    project: "proj_tSNHinT8kJJl8LMqqG8X7HQp",
-    apiKey: "sk-my-service-account-"
+    organization: OPENAI_ORGANIZATION,
+    project: OPENAI_PROJECT,
+    apiKey: OPENAI_API_KEY
 });
 
 async function main() {
@@ -17,7 +17,7 @@ async function main() {
         //prompt: "What is the capital of the United Kingdom?",
         prompt: `Create complete Heta model with stoichiometry 1A + 2D <=> 3C + 4F.`,
         max_tokens: 1024,
-        temperature: 0.0,
+        temperature: 0.1,
     });
     console.log(completion.choices[0].text);
 }
